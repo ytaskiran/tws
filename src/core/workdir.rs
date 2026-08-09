cbe23d47cc3fe56840516dde45aa23d070336d31
ytaskiran <ytaskiran70@gmail.com>
2026-08-10 01:44:02 +0300

    refactor: remove unused expand_tilde helper

--- src/core/workdir.rs
+++ src/core/workdir.rs
@@ -8,17 +8,6 @@ pub fn home_dir() -> PathBuf {
     dirs::home_dir().unwrap_or_else(|| PathBuf::from("/"))
 }
 
-#[allow(dead_code)]
-pub fn expand_tilde(s: &str) -> PathBuf {
-    if s == "~" {
-        return home_dir();
-    }
-    match s.strip_prefix("~/") {
-        Some(rest) => home_dir().join(rest),
-        None => PathBuf::from(s),
-    }
-}
-
 pub fn shorten_home(path: &Path) -> String {
     match path.strip_prefix(home_dir()) {
         Ok(rest) if rest.as_os_str().is_empty() => "~".to_string(),
@@ -199,26 +188,6 @@ mod tests {
     }
 
     #[test]
-    fn expand_tilde_bare() {
-        assert_eq!(expand_tilde("~"), home_dir());
-    }
-
-    #[test]
-    fn expand_tilde_with_subpath() {
-        assert_eq!(expand_tilde("~/projects"), home_dir().join("projects"));
-    }
-
-    #[test]
-    fn expand_tilde_leaves_absolute_paths_alone() {
-        assert_eq!(expand_tilde("/usr/local"), PathBuf::from("/usr/local"));
-    }
-
-    #[test]
-    fn expand_tilde_does_not_expand_mid_string_tilde() {
-        assert_eq!(expand_tilde("/tmp/~foo"), PathBuf::from("/tmp/~foo"));
-    }
-
-    #[test]
     fn shorten_home_replaces_home_prefix() {
         let p = home_dir().join("projects").join("tws");
         assert_eq!(shorten_home(&p), "~/projects/tws");
@@ -232,12 +201,6 @@ mod tests {
     #[test]
     fn shorten_home_leaves_outside_paths_alone() {
         assert_eq!(shorten_home(Path::new("/usr/local")), "/usr/local");
-    }
-
-    #[test]
-    fn shorten_home_round_trips_with_expand_tilde() {
-        let p = home_dir().join("a").join("b");
-        assert_eq!(expand_tilde(&shorten_home(&p)), p);
     }
 
     #[test]
